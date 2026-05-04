27569aa6559fae6ce231fc725b5187af265eadd3
Michael Taye <michaeltaye200@gmail.com>
2026-05-04 15:32:45 +0300

    Update 2

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const http = require('http');
 
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World from Node.js!');
+  res.end('Hello World from Node.js on cPanel!');
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
 });
